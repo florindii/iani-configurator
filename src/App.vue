ad60bce510d4c0c5dd72c332d593b37bdffb0aa3
FLORIND\FlorindLatifi <florind.latifi@rrota.com>
2025-07-16 00:14:12 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import ThreeScene from './components/ThreeScene.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <ThreeScene />
 </template>
 
 <style scoped>
